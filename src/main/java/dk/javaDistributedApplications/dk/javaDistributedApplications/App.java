9d4b0185681aee4c1e7ff6943d76876584ad299f
xiecanhao <513693233@qq.com>
2017-08-24 05:19:00 +0800

    create priject
    创建工程

---
+++ src/main/java/dk/javaDistributedApplications/dk/javaDistributedApplications/App.java
@@ -0,0 +1,13 @@
+package dk.javaDistributedApplications.dk.javaDistributedApplications;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
